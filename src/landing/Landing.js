c9b6aa15808a98f2273071060763315e58a4d5c4
Patrick <janpatrick.dev@gmail.com>
2022-03-25 16:15:04 +0800

    setup auth0, create .env file for auth0 config, create initial home page and logout button on header

--- src/landing/Landing.js
+++ src/landing/Landing.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Button from '../common/Button';
+import LoginButton from '../common/LoginButton';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function Landing() {
 
@@ -9,7 +10,7 @@ function Landing() {
       Welcome to the weather forecast web application. Please login with your Github user
       to use the application and view the weather in your city.
       </p>
-      <Button label="Login" />
+      <LoginButton label="Login" />
     </div>
   );
 }
