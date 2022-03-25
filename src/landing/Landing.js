9777b969f9710420364e9bae2357f4be3b312b9e
Patrick <janpatrick.dev@gmail.com>
2022-03-25 15:17:28 +0800

    create layout for landing page

--- src/landing/Landing.js
+++ src/landing/Landing.js
@@ -1,11 +1,17 @@
 import React from 'react';
-
+import Button from '../common/Button';
 
 function Landing() {
 
   return (
     <div className="landing">
-      
+      <p className="landing__welcome u-margin-bottom-small">
+      Welcome to the weather forecast web application. Please login with your Github user
+      to use the application and view the weather in your city.
+      </p>
+      <Button label="Login" />
     </div>
   );
 }
+
+export default Landing;
